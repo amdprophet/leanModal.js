9c8b1329b73ce382b26c8b3631278fb58f4a38a6
Justin Kolberg <amd.prophet@gmail.com>
2011-10-05 14:02:37 -0700

    now uses scrolling overflow to avoid content from being cut off

--- jquery.leanModal.js
+++ jquery.leanModal.js
@@ -1,68 +1,56 @@
 (function($){
- 
-    $.fn.extend({ 
-         
-        leanModal: function(options) {
- 
-            var defaults = {
-                top: 100,
-                overlay: 0.5
-            }
-                 
-            options =  $.extend(defaults, options);
- 
-            return this.each(function() {
-            
-                var o = options;
-               
-                $(this).click(function(e) {
-              
-          	  	var overlay = $("<div id='lean_overlay'></div>");
-              
-              	var modal_id = $(this).attr("href");
+  $.fn.extend({
+    leanModal: function(options) {
+      var defaults = {
+        top: 100,
+        bottom: 100,
+        overflow: 'scroll',
+        overlay: 0.5
+      }
 
-				$("body").append(overlay);
-                
-				$("#lean_overlay").click(function() { 
-                     close_modal(modal_id);                    
-                });
-                         	
-              	var modal_height = $(modal_id).outerHeight();
-        	  	var modal_width = $(modal_id).outerWidth();
+      options =  $.extend(defaults, options);
 
-        		$('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
+      return this.each(function() {
+        var o = options;
 
-        		$('#lean_overlay').fadeTo(200,o.overlay);
+        $(this).click(function(e) {
+          var overlay = $("<div id='lean_overlay'></div>");
+          var modal_id = $(this).attr("href");
 
-        		$(modal_id).css({ 
-        		
-        			'display' : 'block',
-        			'position' : 'fixed',
-        			'opacity' : 0,
-        			'z-index': 11000,
-        			'left' : 50 + '%',
-        			'margin-left' : -(modal_width/2) + "px",
-        			'top' : o.top + "px"
-        		
-        		});
+          $("body").append(overlay);
 
-        		$(modal_id).fadeTo(200,1);
+          $("#lean_overlay").click(function() {
+            close_modal(modal_id);
+          });
 
-                e.preventDefault();
-                		
-              	});
-             
-            });
+          var modal_height = $(modal_id).outerHeight();
+          var modal_width = $(modal_id).outerWidth();
 
-			function close_modal(modal_id){
+          $('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
+          $('#lean_overlay').fadeTo(200,o.overlay);
 
-        		$("#lean_overlay").fadeOut(200);
+          $(modal_id).css({
+            'display' : 'block',
+            'position' : 'fixed',
+            'opacity' : 0,
+            'z-index': 11000,
+            'left' : 50 + '%',
+            'margin-left' : -(modal_width/2) + "px",
+            'top' : o.top + "px",
+            'bottom' : o.bottom + "px",
+            'overflow' : o.overflow
+          });
 
-        		$(modal_id).css({ 'display' : 'none' });
-			
-			}
-    
-        }
-    });
-     
+          $(modal_id).fadeTo(200,1);
+
+          e.preventDefault();
+        });
+      });
+
+      function close_modal(modal_id){
+        $("#lean_overlay").fadeOut(200);
+        $(modal_id).css({ 'display' : 'none' });
+      }
+    }
+  });
 })(jQuery);
